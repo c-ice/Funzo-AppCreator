607a86e7fba6eeaf95790794bd9a921dea025892
Lukas Chudy <cice@ynet.sk>
2013-02-10 22:25:00 +0100

    aaa

--- Association.js
+++ Association.js
@@ -9,17 +9,20 @@
             var self = this;
             self._target = null;
             self._source = null;
-            self._targetOffset = {'x':0,'y':0};
-            self._sourceOffset = {'x':0,'y':0};
-            self.setDefaultAttrs({
-                points: [0,0,0,0]
-            });
+            self._targetOffset = {
+                'x':0,
+                'y':0
+            };
+            self._sourceOffset = {
+                'x':0,
+                'y':0
+            };
             
             Kinetic.Line.call(self, config);
             this.ACType = 'Association' 
             self.on('click', function() {
                 
-            });
+                });
         },
         setTarget: function(target) {
             var self = this;
@@ -31,7 +34,16 @@
             self._target = target;
             var x = self._target.getWidth()/2 + self._target.getX();
             var y = self._target.getHeight()/2 + self._target.getY();
-            self.setPoints([self.getPoints()[0].x, self.getPoints()[0].y, x, y]);
+            var points = self.getPoints();
+            points.push(new AppCreator.MovePoint(
+            {
+                target: self, 
+                index: 1,
+                'x':x, 
+                'y':y
+            }));
+
+            self.setPoints(points);
             
             self._target.on('dragmove resize', function(){
                 self._calculatePoints();
@@ -51,7 +63,15 @@
             self._source = source;
             var x = self._source.getWidth()/2 + self._source.getX();
             var y = self._source.getHeight()/2 + self._source.getY();
-            self.setPoints([x, y, self.getPoints()[1].x, self.getPoints()[1].y]);
+            var points = self.getPoints();
+            points.push(new AppCreator.MovePoint({
+                target: self, 
+                index: 0,
+                'x':x, 
+                'y':y
+            }));
+
+            self.setPoints(points);
             
             self._source.on('dragmove resize', function(){
                 self._calculatePoints();
@@ -64,29 +84,46 @@
             var points = this.getPoints();
             var len = points.length;
             
-            if (len < 3 && points[0].y != points[len - 1].y) {
-                len = points.push({
-                    x:0,
-                    y:0
-                }, {
-                    x:0,
-                    y:0
-                });
+            if (len < 3 && points[0].getY() != points[len - 1].getY()) {
+                len = points.push(new AppCreator.MovePoint({
+                    target: this, 
+                    index: 2
+                }), 
+                new AppCreator.MovePoint({
+                    target: this, 
+                    index: 3
+                }));
+                points[0].moveTo(this.getParent());
+                points[1].moveTo(this.getParent());
+                points[2].moveTo(this.getParent());
+                points[3].moveTo(this.getParent());
             }
             
-            points[0].x = this.getSource().getWidth()/2 + this.getSource().getX();
-            points[0].y = this.getSource().getHeight()/2 + this.getSource().getY();
+            points[0].setX(this.getSource().getWidth()/2 + this.getSource().getX());
+            points[0].setY(this.getSource().getHeight()/2 + this.getSource().getY());
             
-            points[len - 1].x = this.getTarget().getWidth()/2 + this.getTarget().getX();
-            points[len - 1].y = this.getTarget().getHeight()/2 + this.getTarget().getY();
+            points[len - 1].setX(this.getTarget().getWidth()/2 + this.getTarget().getX());
+            points[len - 1].setY(this.getTarget().getHeight()/2 + this.getTarget().getY());
             
             for (var i = 1; i < len - 1; i++) {
-                points[i].x = points[len - 1].x + (points[0].x - points[len - 1].x)/2;
-                points[i].y = (i > 1)? points[len - 1].y : points[0].y; 
+                points[i].setX(points[len - 1].getX() + (points[0].getX() - points[len - 1].getX())/2);
+                points[i].setY((i > 1)? points[len - 1].getY() : points[0].getY()); 
             }
  
             this.setPoints(points);
             this.getParent().draw();
+        },
+        drawFunc: function(canvas) {
+            var points = this.getPoints(), length = points.length, context = canvas.getContext();
+            context.beginPath();
+            context.moveTo(points[0].getX(), points[0].getY());
+
+            for(var n = 1; n < length; n++) {
+                var point = points[n];
+                context.lineTo(point.getX(), point.getY());
+            }
+
+            canvas.stroke(this);
         }
         
         
--- MovePoint.js
+++ MovePoint.js
@@ -11,13 +11,14 @@
     AppCreator.MovePoint.prototype = {
         _initMovePoint: function(config) {
             var self = this;
-            self._index = 0;
             self._target = config.target;
             self.setDefaultAttrs({
                 radius: 15,
-//                fill: 'black',
-//                opacity: 0.5,
-                draggable: true
+                fill: 'black',
+                opacity: 0.5,
+                draggable: true,
+                index: 0,
+                isStatic: false
             });
             // call super constructor
             Kinetic.Circle.call(self, config);
@@ -42,79 +43,81 @@
             });
             
             self.on('dragmove', function() {
-                var newWidth = self._target.getWidth();
-                var newHeight = self._target.getHeight();
-                if (self.getX() != self.dragStartX) {
-                    var deltaX = self._dragStartX - self.getX();
-                    
-                    if (self._type.id == AppCreator.MovePoint.Type.NorthEast.id || 
-                        self._type.id == AppCreator.MovePoint.Type.SouthEast.id) {
-                        if (this._target.getMinSize().width <= newWidth - deltaX) {
-                            newWidth -= deltaX;
-                        } else {
-                            self.setX(self._dragStartX);
-                        }
-                    } else {
-                        // constraint to minSize
-                        if (this._target.getMinSize().width <= newWidth + deltaX) {
-                            newWidth += deltaX;
-                            self._target.setX(self._target.getX() - deltaX);
-                        } else {
-                            self.setX(self._dragStartX);
-                        }
-                    }
-                    
-                    self._dragStartX = self.getX();
-                }
-                if (self._dragStartY != self.getY()) {
-                    var deltaY = self._dragStartY - self.getY();
-
-                    if (self._type.id == AppCreator.MovePoint.Type.NorthEast.id || 
-                        self._type.id == AppCreator.MovePoint.Type.NorthWest.id) {
-                        // constraint to minSize
-                        if (this._target.getMinSize().height <= newHeight + deltaY) {
-                            newHeight += deltaY;
-                            self._target.setY(self._target.getY() - deltaY);
-                        } else {
-                            self.setY(self._dragStartY);
-                        }
-                    } else {
-                        if (this._target.getMinSize().height <= newHeight - deltaY) {
-                            newHeight -= deltaY;
-                        } else {
-                            self.setY(self._dragStartY);
-                        }
-                    }
-                    
-                    self._dragStartY = self.getY();
-                }
-                
-                self._target.resizeToNewSize(newWidth, newHeight);
+//                var newWidth = self._target.getWidth();
+//                var newHeight = self._target.getHeight();
+//                if (self.getX() != self.dragStartX) {
+//                    var deltaX = self._dragStartX - self.getX();
+//                    
+//                    if (self._type.id == AppCreator.MovePoint.Type.NorthEast.id || 
+//                        self._type.id == AppCreator.MovePoint.Type.SouthEast.id) {
+//                        if (this._target.getMinSize().width <= newWidth - deltaX) {
+//                            newWidth -= deltaX;
+//                        } else {
+//                            self.setX(self._dragStartX);
+//                        }
+//                    } else {
+//                        // constraint to minSize
+//                        if (this._target.getMinSize().width <= newWidth + deltaX) {
+//                            newWidth += deltaX;
+//                            self._target.setX(self._target.getX() - deltaX);
+//                        } else {
+//                            self.setX(self._dragStartX);
+//                        }
+//                    }
+//                    
+//                    self._dragStartX = self.getX();
+//                }
+//                if (self._dragStartY != self.getY()) {
+//                    var deltaY = self._dragStartY - self.getY();
+//
+//                    if (self._type.id == AppCreator.MovePoint.Type.NorthEast.id || 
+//                        self._type.id == AppCreator.MovePoint.Type.NorthWest.id) {
+//                        // constraint to minSize
+//                        if (this._target.getMinSize().height <= newHeight + deltaY) {
+//                            newHeight += deltaY;
+//                            self._target.setY(self._target.getY() - deltaY);
+//                        } else {
+//                            self.setY(self._dragStartY);
+//                        }
+//                    } else {
+//                        if (this._target.getMinSize().height <= newHeight - deltaY) {
+//                            newHeight -= deltaY;
+//                        } else {
+//                            self.setY(self._dragStartY);
+//                        }
+//                    }
+//                    
+//                    self._dragStartY = self.getY();
+//                }
+//                
+//                self._target.resizeToNewSize(newWidth, newHeight);
                 self._target.getParent().draw();
             });
             
-            self._target.on('dragmove', function() {
-                self.setType(self._type);
-            });
-            
-            self._target.on('dragstart', function(){
-//                var old = self.getParent();
-//                self.moveTo(this.getParent());
-//                old.draw();
-            });
-            
-            self._target.on('dragend', function(){
-//                self.moveTo(this.getParent());
-//                this.getParent().draw();
-            });
-            
-            self._target.on('widthChange', function(evt) {
-                  if (!self._dragStartX) {
-                      self.setType(self._type);
-                  }
-            });
+//            self._target.on('dragmove', function() {
+//                self.setType(self._type);
+//            });
+//            
+//            self._target.on('dragstart', function(){
+////                var old = self.getParent();
+////                self.moveTo(this.getParent());
+////                old.draw();
+//            });
+//            
+//            self._target.on('dragend', function(){
+////                self.moveTo(this.getParent());
+////                this.getParent().draw();
+//            });
+//            
+//            self._target.on('widthChange', function(evt) {
+//                  if (!self._dragStartX) {
+//                      self.setType(self._type);
+//                  }
+//            });
         }
     }
     
     Kinetic.Global.extend(AppCreator.MovePoint, Kinetic.Circle);
+    
+    Kinetic.Node.addGettersSetters(Kinetic.Circle, ['index', 'isStatic']);
 })();
