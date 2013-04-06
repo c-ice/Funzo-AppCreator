94dda6b40fe41f717faf60159f33f2d61d378b57
cice@ynet.sk <>
2013-04-06 22:41:07 +0200

    Adding AppCreator.CFG. config namespace where default configurations will be stored

---
+++ js/utils/Configuration.js
@@ -0,0 +1,10 @@
+(function() {
+    AppCreator.CFG = {};
+    
+    AppCreator.CFG.primitiveDataTypes = ['String', 'Number', 'Bool', 'Reference'];
+    
+    AppCreator.CFG.ModelView = {};
+    
+    AppCreator.CFG.presettedAttributes = [{"name":"ID", "type":"Number"}];
+})();
+
