509d6f8c104775deea1859230a5b884bbab7b707
Lukas Chudy <cice@ynet.sk>
2013-04-14 21:59:40 +0200

    a Lot of changes for drawing Names and Counting of connected element over association

--- js/Association.js
+++ js/Association.js
@@ -13,6 +13,8 @@
             this._source = null;
             this._sourceNameText = null;
             this._sourceCountingText = null;
+            this._targetNameText = null;
+            this._targetCountingText = null;
 
             Kinetic.Line.call(this, config);
 
@@ -25,26 +27,104 @@
                 point.getParent().draw();
                 point.fire('mousedown');
             });
+
+            this._dblClickCountingFunc = function(e) {
+                var dlg = new AppCreator.Dialogs.CountingTextDialog({
+                    y: this.getAbsolutePosition().y,
+                    x: this.getAbsolutePosition().x,
+                    width: 60,
+                }), that = this;
+                self.getSource().setDraggable(false);
+                self.getSource().setNotDraggable(true);
+                dlg.submit = function(el) {
+                    $(el).serializeObject();
+                    that.setText($(el).serializeObject().counting);
+                    dlg.remove();
+                    self.getSource().setNotDraggable(false);
+                    self.getSource().setDraggable(true);
+                    that.getParent().draw();
+                };
+                dlg.cancel = function() {
+                    self.getSource().setNotDraggable(false);
+                    self.getSource().setDraggable(true);
+                    this.remove();
+                };
+            };
             
+            this._dblClickNameFunc = function(e) {
+                var dlg = new AppCreator.Dialogs.TitleDialog({
+                    y: this.getAbsolutePosition().y,
+                    x: this.getAbsolutePosition().x,
+                    width: 60,
+                    title: this.getText(), 
+                    placeholder: "Association"
+                }), that = this;
+                
+                self.getSource().setDraggable(false);
+                self.getSource().setNotDraggable(true);
+                dlg.submit = function(el) {
+                    $(el).serializeObject();
+                    that.setText($(el).serializeObject().name);
+                    dlg.remove();
+                    self.getSource().setNotDraggable(false);
+                    self.getSource().setDraggable(true);
+                    that.getParent().draw();
+                };
+                dlg.cancel = function() {
+                    self.getSource().setNotDraggable(false);
+                    self.getSource().setDraggable(true);
+                    this.remove();
+                };
+            };
+
+            this._setupTextes();
+        },
+        _setupTextes: function() {
             this._sourceNameText = new Kinetic.Text({
-                    // x,y when drawing
-                    text: "-class",
-                    fontSize: 13,
-                    fontFamily: 'Calibri',
-                    fill: 'black',
-                    align: 'center',
-                    width: 50
+                // x,y when drawing
+                text: "-assoc",
+                fontSize: 14,
+                fontFamily: 'Calibri',
+                fill: 'black',
+                align: 'center',
+                width: 60
             });
-            
+
             this._sourceCountingText = new Kinetic.Text({
-                    // x,y when drawing
-                    text: "*",
-                    fontSize: 14,
-                    fontFamily: 'Calibri',
-                    fill: 'black',
-                    align: 'center',
-                    width: 50
+                // x,y when drawing
+                text: "*",
+                fontSize: 14,
+                fontFamily: 'Calibri',
+                fill: 'black',
+                align: 'center',
+                width: 28
             });
+
+            this._targetNameText = new Kinetic.Text({
+                // x,y when drawing
+                text: "-assoc",
+                fontSize: 14,
+                fontFamily: 'Calibri',
+                fill: 'black',
+                align: 'center',
+                width: 60
+            });
+
+            this._targetCountingText = new Kinetic.Text({
+                // x,y when drawing
+                text: "*",
+                fontSize: 14,
+                fontFamily: 'Calibri',
+                fill: 'black',
+                align: 'center',
+                width: 28
+            });
+
+            // doubleClicks
+            this._sourceCountingText.on('dblclick', this._dblClickCountingFunc);
+            this._targetCountingText.on('dblclick', this._dblClickCountingFunc);
+            this._sourceNameText.on('dblclick', this._dblClickNameFunc);
+            this._targetNameText.on('dblclick', this._dblClickNameFunc);
         },
         addMovePoint: function() {
             //TODO: zatial sa tahaju nepriamociaro mozno to prerobit ?
@@ -92,30 +172,30 @@
          * @param {type} newPoint
          * @returns {unresolved}
          */
-        addOrGetBreakPoint: function(newPoint) {
-            for (var breakPoint in breakPoints) {
-                if (GraphicsTools.isPointNearPoint(newPoint, breakPoint, nearTolerance)) {
-                    return breakPoint;
-                }
-            }
-
-            if (breakPoints.isEmpty()) {
-                breakPoints.add(newPoint);
-            } else {
-                var previous = getStart();
-                for (var i = 0; i < breakPoints.size(); i++) {
-                    if (GraphicsTools.isPointNearSegment(previous, breakPoints.get(i), newPoint, nearTolerance)) {
-                        breakPoints.add(i, newPoint);
-                        return newPoint;
-                    }
-                    previous = breakPoints.get(i);
-                }
-                if (GraphicsTools.isPointNearSegment(previous, getEnd(), newPoint, nearTolerance)) {
-                    breakPoints.add(newPoint);
-                }
-            }
-            return newPoint;
-        },
+//        addOrGetBreakPoint: function(newPoint) {
+//            for (var breakPoint in breakPoints) {
+//                if (GraphicsTools.isPointNearPoint(newPoint, breakPoint, nearTolerance)) {
+//                    return breakPoint;
+//                }
+//            }
+//
+//            if (breakPoints.isEmpty()) {
+//                breakPoints.add(newPoint);
+//            } else {
+//                var previous = getStart();
+//                for (var i = 0; i < breakPoints.size(); i++) {
+//                    if (GraphicsTools.isPointNearSegment(previous, breakPoints.get(i), newPoint, nearTolerance)) {
+//                        breakPoints.add(i, newPoint);
+//                        return newPoint;
+//                    }
+//                    previous = breakPoints.get(i);
+//                }
+//                if (GraphicsTools.isPointNearSegment(previous, getEnd(), newPoint, nearTolerance)) {
+//                    breakPoints.add(newPoint);
+//                }
+//            }
+//            return newPoint;
+//        },
         cleanupUnecessaryBreakPoints: function() {
             var points = this.getPoints(), i = 1,
                     tolerance = AppCreator.GraphicTools.tolerance / 2;
@@ -149,7 +229,7 @@
                 self._target = null;
             }
 
-            if (self.getSource() && target.getX() < self.getSource().getX()) {
+            if (self.getSource() && target.getX() <= self.getSource().getX()) {
                 x = target.getWidth() + target.getX();
                 this.setTargetOffset({
                     'x': target.getWidth(),
@@ -176,8 +256,11 @@
             });
 
             self._target = target;
-            
+
             self.getParent().add(this._sourceCountingText);
+            self.getParent().add(this._sourceNameText);
+            self.getParent().add(this._targetCountingText);
+            self.getParent().add(this._targetNameText);
         },
         getTarget: function() {
             return this._target;
@@ -188,53 +271,64 @@
             if (self._source) {
                 self._source.off('dragmove');
                 self._source.off('resize');
+                self._source = null;
             }
-            self._source = source;
 
             if (self.getTarget()) {
-                xOffset = self.getTarget().getX() > self._source.getX() ? self._source.getWidth() : 0;
+                xOffset = self.getTarget().getX() > source.getX() ? source.getWidth() : 0;
             } else {
-                xOffset = self._source.getWidth();
+                xOffset = source.getWidth();
             }
 
             this.setSourceOffset({
                 'x': xOffset,
-                'y': self._source.getHeight() / 2
+                'y': source.getHeight() / 2
             });
 
-            x = self._source.getX() + xOffset;
-            y = self._source.getHeight() / 2 + self._source.getY();
+            x = source.getX() + xOffset;
+            y = source.getHeight() / 2 + source.getY();
 
             point = self.addMovePoint([x, y]);
             point.setIsStatic(true);
-            point.setTarget(self._source);
+            point.setTarget(source);
 
-            self._source.on('dragmove resize', function() {
+            source.on('dragmove resize', function() {
                 point.setX(this.getX() + self.getSourceOffset().x);
                 point.setY(this.getY() + self.getSourceOffset().y);
                 self.getParent().draw();
             });
+            
+            self._source = source;
         },
         getSource: function() {
             return this._source;
         },
         drawFunc: function(canvas) {
-            var points = this.getPoints(), length = points.length, context = canvas.getContext();
+            var points = this.getPoints(), length = points.length,
+                    context = canvas.getContext(), point, n = 1;
+
             context.beginPath();
             context.moveTo(points[0].getX(), points[0].getY());
             points[0].moveToTop();
 
-            for (var n = 1; n < length; n++) {
-                var point = points[n];
+            for (; n < length; n++) {
+                point = points[n];
                 context.lineTo(point.getX(), point.getY());
             }
 
-            //TODO: add drawing off text;
-            if (this._sourceCountingText)
-                this._sourceCountingText.setPosition([points[0].getX(), points[0].getY() - 10]);
+            if (this.getSource() && this._sourceCountingText) {
+                this._adjustTextPosition(this._sourceCountingText, points[0], this.getSource(), -20);
+                this._adjustTextPosition(this._sourceNameText, points[0], this.getSource(), 10);
+            }
+
+            if (this.getTarget() && this._targetCountingText) {
+                this._adjustTextPosition(this._targetCountingText, points[length - 1], this.getTarget(), -20);
+                this._adjustTextPosition(this._targetNameText, points[length - 1], this.getTarget(), 10);
+            }
 
             canvas.stroke(this);
-        },
+        }
+        ,
         drawHitFunc: function(canvas) {
             var points = this.getPoints(), length = points.length, context = canvas.getContext();
             context.beginPath();
@@ -248,6 +342,30 @@
             this.setStrokeWidth(10);
             canvas.stroke(this);
             this.setStrokeWidth(1);
+        },
+        /**
+         * set Position relativly to element
+         * @param {type} text
+         * @param {type} point
+         * @param {type} element
+         * @returns {undefined}
+         */
+        _adjustTextPosition: function(text, point, element, yOffset) {
+            var xOffset = 10;
+            if (Math.abs(element.getX() - point.getX()) < 10) {
+                xOffset = -10 - text.getWidth() | 0;
+            }
+
+            text.setPosition([point.getX() + xOffset, point.getY() + yOffset]);
+            if (xOffset > 0) {
+                text.setAlign('left');
+            } else {
+                text.setAlign('right');
+            }
+
+//            rotation
+//            var rotation = AppCreator.GraphicTools.radiansFromPoints(points[0], points[1]) - (points[0].getX() > points[1].getX()? Math.PI:0);
+//             this._sourceCountingText.setRotation(rotation*((points[0].getY() < points[1].getY()) ? 1:-1));
         }
     };
 
@@ -255,10 +373,10 @@
 
     Kinetic.Node.addGetterSetter(AppCreator.Association, 'targetOffset', {x: 0, y: 0});
     Kinetic.Node.addGetterSetter(AppCreator.Association, 'sourceOffset', {x: 0, y: 0});
-    
+
     Kinetic.Node.addGetterSetter(AppCreator.Association, 'sourceName');
     Kinetic.Node.addGetterSetter(AppCreator.Association, 'targetName');
-    
+
     Kinetic.Node.addGetterSetter(AppCreator.Association, 'targetCounting');
     Kinetic.Node.addGetterSetter(AppCreator.Association, 'sourceCounting');
 })();
